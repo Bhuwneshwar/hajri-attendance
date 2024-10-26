e6cf557a1cbad68b6eb05881ac6b1fe8ebab8e4e
Bhuwneshwar mandal <krabi6563@gmail.com>
2024-10-26 13:48:06 +0530

    deploy on railway

--- server/models/Hajri.js
+++ server/models/Hajri.js
@@ -1,29 +1,17 @@
 const mongoose = require("mongoose");
 
 const Schema = new mongoose.Schema(
-<<<<<<< HEAD
   {
     date: { type: String, required: true }, //Optional
     rate: { type: Number, required: true }, //Optional
     paid: { type: Number, required: true },
     due: { type: Number },
     builder: { type: String },
-    userId: { type: String, uniqed: true },
+    userId: { type: String, unique: true },
     contact: { type: String },
     location: { type: String },
   },
   { timestamps: true }
-=======
-    {
-        date: { type: String, required: true }, //Optional
-        rate: { type: Number, required: true }, //Optional
-        paid: { type: Number, required: true },
-        due: { type: Number },
-        builder: { type: String },
-        location: { type: String }
-    },
-    { timestamps: true }
->>>>>>> 52a7827ccf9d49d7ff815adbfda8df13bf67db9f
 );
 
 module.exports = mongoose.model("Attendance", Schema);
